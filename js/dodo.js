12bc0394ef7658f475f74146fa13c19064e3d725
dodo <dodo45133@gmail.com>
2023-05-07 02:34:26 +0900

    test

--- js/dodo.js
+++ js/dodo.js
@@ -1,23 +1,31 @@
 (() => {
-    $('#toggle-on').click(function() {
+    $('#toggle-on').click(function () {
         $('#white-mode').fadeOut()
         $('#dark-mode').fadeIn()
     });
 
-    $('#toggle-off').click(function() {
+    $('#toggle-off').click(function () {
         $('#white-mode').fadeIn()
         $('#dark-mode').fadeOut()
     });
 
 
     $(".owl-carousel").owlCarousel({
-        margin:10,
-        loop:true,
-        autoWidth:true,
-        items:4,
+        margin: 10,
+        loop: true,
+        autoWidth: true,
+        items: 4,
         // nav: true,
         dots: true,
     });
+
+    var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    var options = { //지도를 생성할 때 필요한 기본 옵션
+        center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+        level: 3 //지도의 레벨(확대, 축소 정도)
+    };
+
+    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
     console.log("ready")
 })();
